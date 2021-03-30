dfa4551a441313e51882e19b7331719e46e5641d
belbahi <belbahiakram@gmail.com>
2021-03-30 17:17:22 +0200

    add a new api

--- src/main/java/com/sundaydevblog/springrestapitest/controller/MemberController.java
+++ src/main/java/com/sundaydevblog/springrestapitest/controller/MemberController.java
@@ -72,7 +72,7 @@ public class MemberController {
 
     @GetMapping(value = "/hello")
     public String sayHi(){
-        return "Say hello to my little friend !, akram!";
+        return "Say hello to my little friend !, akram belbahi!";
     }
 
 }
